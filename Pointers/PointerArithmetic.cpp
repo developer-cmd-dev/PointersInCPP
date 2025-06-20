9b42945e4b38f66bf8eab0bf6ca0a231fa5734cd
Dev Kumar <devkmandal0@gmail.com>
2025-06-20 19:35:17 +0530

    Add PointerArithmetic.cpp demonstrating pointer increment, decrement, and array access

---
+++ Pointers/PointerArithmetic.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+// Increment(++) and decrement(--)
+
+int a = 56;
+int *ptr = &a;
+cout<<ptr<<endl;
+ptr++;
+cout<<ptr<<endl;
+ptr--;
+cout<<ptr<<endl;
+
+
+int arr[] ={1,2,3,4,5,6};
+
+cout<<*arr<<endl;
+cout<<*(arr+1)<<endl;
+cout<<*(arr+2)<<endl;
+cout<<*(arr+3)<<endl;
+cout<<*(arr+4)<<endl;
+cout<<*(arr+5)<<endl;
+
+
+
+
+
+    
+
+
+    return 0;
+}
